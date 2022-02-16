da8d4e67e6fe4c3ac7122e19e21bde224bb5a871
Miguel <hp0826022019@unab.edu.sv>
2022-02-15 19:50:00 -0600

    nuevo proyecto c++

---
+++ ClassSuma.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+bool suma(){
+	bool a =0, b=0, c=0;
+	cout<<"dame un numero:"<<endl;cin>>a;
+	cout<<"dame un segundo numero:"<<endl;cin>>b;
+	
+	cout<<"El resultado es:"<<endl; c = a+b;
+	system("pause");
+	return c;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include "ClassSuma.h"
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	suma();
+	
+	return 0;
+}
